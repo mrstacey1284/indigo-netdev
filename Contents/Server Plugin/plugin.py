9818a222b39761286231545ec7cc8a0d68406d93
jheddings <jason@heddings.com>
2015-05-09 22:36:18 -0600

    Many improvements to the ARP cache and retry handling.

--- Contents/Server Plugin/plugin.py
+++ Contents/Server Plugin/plugin.py
@@ -1,6 +1,10 @@
 #! /usr/bin/env python
 
-import re, subprocess
+import time
+import subprocess
+
+# NOTE most of the pending improvements here are in building the ARP cache
+# could add some additional status checks on active devices, e.g. ping
 
 ################################################################################
 class Plugin(indigo.PluginBase):
@@ -9,9 +13,12 @@ class Plugin(indigo.PluginBase):
     def __init__(self, pluginId, pluginDisplayName, pluginVersion, pluginPrefs):
         indigo.PluginBase.__init__(self, pluginId, pluginDisplayName, pluginVersion, pluginPrefs)
 
+        # TODO validate UI config options
+        self.retryCount = int(pluginPrefs.get('retryCount', 5))
+        self.retryInterval = int(pluginPrefs.get('retryInterval', 60))
         self.debug = pluginPrefs.get('debug', False)
-        self.devices = [ ]
-        self.arp_table = None
+
+        self.arp_cache = None
 
     #---------------------------------------------------------------------------
     def __del__(self):
@@ -19,25 +26,25 @@ class Plugin(indigo.PluginBase):
 
     #---------------------------------------------------------------------------
     def startup(self):
-        self.debugLog('Plugin starting')
+        self.debugLog('Plugin startup')
+        self.rebuildArpCache()
 
     #---------------------------------------------------------------------------
     def shutdown(self):
-        self.debugLog('Plugin stopping')
+        self.debugLog('Plugin shutdown')
+
+    #---------------------------------------------------------------------------
+    def didDeviceCommPropertyChange(self, origDev, newDev):
+        return origDev.pluginProps['address'] != newDev.pluginProps['address']
 
     #---------------------------------------------------------------------------
     def deviceStartComm(self, device):
-        self.debugLog('Starting device: ' + device.name)
-
-        if device.id not in self.devices:
-            self.devices.append(device.id)
+        self.debugLog('Starting device comm: ' + device.name)
+        self.updateDeviceStates(device)
 
     #---------------------------------------------------------------------------
     def deviceStopComm(self, device):
-        self.debugLog('Stopping device: ' + device.name)
-
-        if device.id in self.devices:
-            self.devices.remove(device.id)
+        self.debugLog('Stopping device comm: ' + device.name)
 
     #---------------------------------------------------------------------------
     def runConcurrentThread(self):
@@ -46,42 +53,86 @@ class Plugin(indigo.PluginBase):
         try:
 
             while True:
+                # devices are updated when added, so we'll start with a sleep
+                self.sleep(self.retryInterval)
+
                 # grab the current arp table
-                self.arp_table = self.findDevices()
-                self.debugLog('ARP table: ' + str(self.arp_table))
+                self.rebuildArpCache()
 
-                for id in self.devices:
-                    self.update(indigo.devices[id])
+                # update all active and configured devices
+                for device in indigo.devices.itervalues('self'):
+                    if device and device.configured:
+                        self.updateDeviceStates(device)
 
-                # TODO make the interval configurable
-                self.sleep(60)
+                # sleep until the next check
+                self.debugLog('Thread Sleep: ' + str(self.retryInterval))
 
-        except: pass
+        except self.StopThread: pass
 
         self.debugLog('Thread Stopped')
 
     #---------------------------------------------------------------------------
-    def update(self, device):
-        if device.address in self.arp_table:
-            self.debugLog('Device Found: ' + device.name)
-            device.updateStateOnServer('present', True)
-            device.updateStateOnServer('status', 'present')
+    def updateDeviceStates(self, device):
+        self.debugLog('Update Device: ' + device.name)
+        props = device.pluginProps
+
+        # the device is in the current arp cache
+        if any(entry['eth_addr'] == device.address for entry in self.arp_cache):
+            self.debugLog('  - Device is ACTIVE')
+
+            device.updateStateOnServer('active', True)
+            device.updateStateOnServer('status', 'Active')
+            device.updateStateOnServer('lastSeenAt', time.strftime('%c'))
+
+            props['retry'] = self.retryCount
+
+        # the device isn't there, but we have retries remaining...
+        elif props.get('retry', 0) > 0:
+            self.debugLog('  - Retry Device: ' + str(props['retry']))
+
+            props['retry'] -= 1
+
+        # the device isn't there and we are out of retries
         else:
-            self.debugLog('Device NOT Found: ' + device.name)
-            device.updateStateOnServer('present', False)
-            device.updateStateOnServer('status', 'away')
+            self.debugLog('  - Device is NOT Active')
+
+            device.updateStateOnServer('active', False)
+            device.updateStateOnServer('status', 'Inactive')
+
+            props['retry'] = 0
+
+        device.replacePluginPropsOnServer(props)
 
     #---------------------------------------------------------------------------
-    def findDevices(self):
-        # XXX make these plugin config params
+    def rebuildArpCache(self):
+        # TODO make these plugin config params
         router_user = 'root'
         router_addr = '10.0.0.1'
         router_passwd = None
-
         cmd = ['/usr/bin/ssh', router_user + '@' + router_addr, 'cat /proc/net/arp']
-        proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        arp_data = proc.communicate()[0]
 
-        # XXX could use better error handling here...
-        return [line.split()[3].upper() for line in arp_data.splitlines()]
+        # XXX the local arp table doesn't seem as reliable as the router,
+        # but it would be much nicer to avoid depending on ssh
+        #cmd = ['/usr/sbin/arp', '-a']
+
+        proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        pout, perr = proc.communicate()
+
+        cache = [ ]
+
+        # this table is dependent on the mthod above
+        # FIXME need to trim non-arp lines (e.g. table header)
+        for line in pout.splitlines():
+            parts = line.split()
+            entry = {
+                'hostname': None,
+                'ip_addr': parts[0],
+                'eth_addr': parts[3].upper(),
+                'iface': parts[5]
+            }
+
+            cache.append(entry)
+            self.debugLog('ARP: ' + str(entry))
+
+        self.arp_cache = cache
 
