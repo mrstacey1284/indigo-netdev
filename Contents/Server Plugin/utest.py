e4feb441a800f89fa5d1fa182418ccdb58521aea
jheddings <jason@heddings.com>
2017-01-02 12:38:42 -0700

    Documentation update

--- Contents/Server Plugin/utest.py
+++ Contents/Server Plugin/utest.py
@@ -7,6 +7,7 @@ import clients
 # keep logging output to a minumim for testing
 logging.basicConfig(level=logging.ERROR)
 
+################################################################################
 class GoogleDNSService(unittest.TestCase):
 
     def setUp(self):
@@ -16,6 +17,7 @@ class GoogleDNSService(unittest.TestCase):
         available = self.client.isAvailable()
         self.assertTrue(available)
 
+################################################################################
 class BasicLocalCommand(unittest.TestCase):
 
     def setUp(self):
@@ -25,6 +27,7 @@ class BasicLocalCommand(unittest.TestCase):
         available = self.client.isAvailable()
         self.assertTrue(available)
 
+################################################################################
 class LocalHostSSH(unittest.TestCase):
 
     def setUp(self):
