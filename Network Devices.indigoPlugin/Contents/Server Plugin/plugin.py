3e18e41c4e68bc08b73ab1712dd52f6c86d11972
jheddings <jason@heddings.com>
2018-04-29 07:43:07 -0600

    Moved device wrappers to module

--- Network Devices.indigoPlugin/Contents/Server Plugin/plugin.py
+++ Network Devices.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -2,9 +2,9 @@
 
 import logging
 import socket
-import urllib2
 import subprocess
 
+import wrapper
 import clients
 
 ################################################################################
@@ -50,6 +50,8 @@ def validateConfig_Int(key, values, errors, min=None, max=None):
 class Plugin(indigo.PluginBase):
 
     wrappers = dict()
+
+    # TODO this might be a new class that is shared by Local device types
     arp_cache = None
 
     #---------------------------------------------------------------------------
@@ -65,8 +67,8 @@ class Plugin(indigo.PluginBase):
     def refreshAllDevices(self):
         # update all enabled and configured devices
         for id in self.wrappers:
-            wrapper = self.wrappers[id]
-            wrapper.updateStatus()
+            wrap = self.wrappers[id]
+            wrap.updateStatus()
 
     #---------------------------------------------------------------------------
     def rebuildArpCache(self):
@@ -108,17 +110,17 @@ class Plugin(indigo.PluginBase):
         errors = indigo.Dict()
 
         if typeId == 'service':
-            DeviceWrapper_Service.validateConfig(values, errors)
+            wrapper.Service.validateConfig(values, errors)
         elif typeId == 'ping':
-            DeviceWrapper_Ping.validateConfig(values, errors)
+            wrapper.Ping.validateConfig(values, errors)
         elif typeId == 'http':
-            DeviceWrapper_HTTP.validateConfig(values, errors)
+            wrapper.HTTP.validateConfig(values, errors)
         elif typeId == 'local':
-            DeviceWrapper_Local.validateConfig(values, errors)
+            wrapper.Local.validateConfig(values, errors)
         elif typeId == 'ssh':
-            DeviceWrapper_SSH.validateConfig(values, errors)
+            wrapper.SSH.validateConfig(values, errors)
         elif typeId == 'macos':
-            DeviceWrapper_macOS.validateConfig(values, errors)
+            wrapper.macOS.validateConfig(values, errors)
 
         return ((len(errors) == 0), values, errors)
 
@@ -133,30 +135,30 @@ class Plugin(indigo.PluginBase):
 
         self.logger.debug(u'Starting device - %s [%s]', device.name, typeId)
 
-        wrapper = None
+        wrap = None
 
         if typeId == 'service':
-            wrapper = DeviceWrapper_Service(device)
+            wrap = wrapper.Service(device)
         elif typeId == 'ping':
-            wrapper = DeviceWrapper_Ping(device)
+            wrap = wrapper.Ping(device)
         elif typeId == 'http':
-            wrapper = DeviceWrapper_HTTP(device)
+            wrap = wrapper.HTTP(device)
         elif typeId == 'local':
-            wrapper = DeviceWrapper_Local(device)
+            wrap = wrapper.Local(device)
         elif typeId == 'ssh':
-            wrapper = DeviceWrapper_SSH(device)
+            wrap = wrapper.SSH(device)
         elif typeId == 'macos':
-            wrapper = DeviceWrapper_macOS(device)
+            wrap = wrapper.macOS(device)
         else:
             self.logger.error(u'unknown device type: %s', typeId)
 
-        self.wrappers[device.id] = wrapper
+        self.wrappers[device.id] = wrap
 
         # XXX we might want to make sure the device status is updated here...
         # the problem with that is it makes for a long plugin startup if all
         # devices update status - especially things like ping and http.
         # !! when status is updated here, start the thread loop with a sleep !!
-        #if device.configured: wrapper.updateStatus()
+        #if device.configured: wrap.updateStatus()
 
     #---------------------------------------------------------------------------
     def deviceStopComm(self, device):
@@ -219,22 +221,22 @@ class Plugin(indigo.PluginBase):
         act = action.deviceAction
         self.logger.debug(u'actionControlDimmerRelay[%s] - %s', act, device.name)
 
-        wrapper = self.wrappers[device.id]
+        wrap = self.wrappers[device.id]
 
         #### TURN ON ####
         if act == indigo.kDimmerRelayAction.TurnOn:
-            wrapper.turnOn()
+            wrap.turnOn()
 
         #### TURN OFF ####
         elif act == indigo.kDimmerRelayAction.TurnOff:
-            wrapper.turnOff()
+            wrap.turnOff()
 
         #### TOGGLE ####
         elif act == indigo.kDimmerRelayAction.Toggle:
             if device.onState:
-                wrapper.turnOff()
+                wrap.turnOff()
             else:
-                wrapper.turnOn()
+                wrap.turnOn()
 
     #---------------------------------------------------------------------------
     # General Action callback
@@ -242,214 +244,13 @@ class Plugin(indigo.PluginBase):
         act = action.deviceAction
         self.logger.debug(u'actionControlGeneral[%s] - %s', act, device.name)
 
-        wrapper = self.wrappers[device.id]
+        wrap = self.wrappers[device.id]
 
         #### STATUS REQUEST ####
         if act == indigo.kDeviceGeneralAction.RequestStatus:
-            wrapper.updateStatus()
+            wrap.updateStatus()
 
         #### BEEP ####
         elif act == indigo.kDeviceGeneralAction.Beep:
             pass
-
-################################################################################
-# wrapper base class for device types
-class DeviceWrapper():
-
-    #---------------------------------------------------------------------------
-    def __init__(self, device):
-        raise NotImplementedError()
-
-    #---------------------------------------------------------------------------
-    # sub-classes should override this for their specific device states
-    def updateStatus(self):
-        device = self.device
-
-        if self.client.isAvailable():
-            self.logger.debug(u'%s is AVAILABLE', device.name)
-            device.updateStateOnServer('active', True)
-            device.updateStateOnServer('status', 'Active')
-
-        else:
-            self.logger.debug(u'%s is UNAVAILABLE', device.name)
-            device.updateStateOnServer('active', False)
-            device.updateStateOnServer('status', 'Inactive')
-
-################################################################################
-# base wrapper class for relay-type devices
-class RelayDeviceWrapper(DeviceWrapper):
-
-    #---------------------------------------------------------------------------
-    def __init__(self, device):
-        raise NotImplementedError()
-
-    #---------------------------------------------------------------------------
-    # default behavior; subclasses should provide correct implementation
-    def turnOff(self):
-        self.logger.warn(u'Not supported - Turn Off %s', self.device.name)
-
-    #---------------------------------------------------------------------------
-    # default behavior; subclasses should provide correct implementation
-    def turnOn(self):
-        self.logger.warn(u'Not supported - Turn On %s', self.device.name)
-
-    #---------------------------------------------------------------------------
-    # basic check to see if the server is responding
-    def updateStatus(self):
-        device = self.device
-
-        if self.client.isAvailable():
-            self.logger.debug(u'%s is AVAILABLE', device.name)
-            device.updateStateOnServer('onOffState', 'on')
-        else:
-            self.logger.debug(u'%s is UNAVAILABLE', device.name)
-            device.updateStateOnServer('onOffState', 'off')
-
-################################################################################
-# plugin device wrapper for Network Service devices
-class DeviceWrapper_Service(DeviceWrapper):
-
-    #---------------------------------------------------------------------------
-    def __init__(self, device):
-        # to emit Indigo events, logger must be a child of 'Plugin'
-        self.logger = logging.getLogger('Plugin.DeviceWrapper_Service')
-
-        address = device.pluginProps['address']
-        port = int(device.pluginProps['port'])
-        client = clients.ServiceClient(address, port)
-
-        self.device = device
-        self.client = client
-
-    #---------------------------------------------------------------------------
-    @staticmethod
-    def validateConfig(values, errors):
-        validateConfig_String('address', values, errors, emptyOk=False)
-        validateConfig_Int('port', values, errors, min=1, max=65536)
-
-################################################################################
-# plugin device wrapper for Ping Status devices
-class DeviceWrapper_Ping(DeviceWrapper):
-
-    #---------------------------------------------------------------------------
-    def __init__(self, device):
-        # to emit Indigo events, logger must be a child of 'Plugin'
-        self.logger = logging.getLogger('Plugin.DeviceWrapper_Ping')
-
-        address = device.pluginProps['address']
-
-        self.device = device
-        self.client = clients.PingClient(address)
-
-    #---------------------------------------------------------------------------
-    @staticmethod
-    def validateConfig(values, errors):
-        validateConfig_String('address', values, errors, emptyOk=False)
-
-################################################################################
-# plugin device wrapper for HTTP Status devices
-class DeviceWrapper_HTTP(DeviceWrapper):
-
-    #---------------------------------------------------------------------------
-    def __init__(self, device):
-        # to emit Indigo events, logger must be a child of 'Plugin'
-        self.logger = logging.getLogger('Plugin.DeviceWrapper_HTTP')
-
-        url = device.pluginProps['url']
-
-        self.device = device
-        self.client = clients.HttpClient(url)
-
-    #---------------------------------------------------------------------------
-    @staticmethod
-    def validateConfig(values, errors):
-        validateConfig_String('url', values, errors, emptyOk=False)
-
-        # update 'address' for proper display
-        url = values['url']
-        req = urllib2.Request(url)
-
-        values['address'] = req.get_host()
-
-################################################################################
-# plugin device wrapper for Local Device types
-class DeviceWrapper_Local(DeviceWrapper):
-
-    #---------------------------------------------------------------------------
-    def __init__(self, device):
-        # to emit Indigo events, logger must be a child of 'Plugin'
-        self.logger = logging.getLogger('Plugin.DeviceWrapper_Local')
-
-        address = device.pluginProps['address']
-
-        self.device = device
-        self.client = clients.NullClient()
-
-    #---------------------------------------------------------------------------
-    @staticmethod
-    def validateConfig(values, errors):
-        validateConfig_String('address', values, errors, emptyOk=False)
-
-################################################################################
-# plugin device wrapper for SSH Device types
-class DeviceWrapper_SSH(RelayDeviceWrapper):
-
-    #---------------------------------------------------------------------------
-    def __init__(self, device):
-        # to emit Indigo events, logger must be a child of 'Plugin'
-        self.logger = logging.getLogger('Plugin.DeviceWrapper_SSH')
-
-        address = device.pluginProps['address']
-        port = int(device.pluginProps['port'])
-        uname = device.pluginProps['username']
-        client = clients.SSHClient(address, port=port, username=uname)
-
-        client.commands['status'] = device.pluginProps['cmd_status']
-        client.commands['shutdown'] = device.pluginProps['cmd_shutdown']
-
-        self.client = client
-        self.device = device
-
-    #---------------------------------------------------------------------------
-    @staticmethod
-    def validateConfig(values, errors):
-        DeviceWrapper_Service.validateConfig(values, errors)
-        validateConfig_String('cmd_status', values, errors, emptyOk=False)
-        validateConfig_String('cmd_shutdown', values, errors, emptyOk=False)
-
-    #---------------------------------------------------------------------------
-    def turnOff(self):
-        device = self.device
-        self.logger.info(u'Shutting down %s', device.name)
-
-        if not self.client.turnOff():
-            self.logger.error(u'Could not turn off remote server: %s', device.name)
-
-################################################################################
-# plugin device wrapper for macOS Device types
-class DeviceWrapper_macOS(RelayDeviceWrapper):
-
-    # XXX could we use remote management instead of SSH?
-
-    #---------------------------------------------------------------------------
-    def __init__(self, device):
-        # to emit Indigo events, logger must be a child of 'Plugin'
-        self.logger = logging.getLogger('Plugin.DeviceWrapper_macOS')
-
-        address = device.pluginProps['address']
-        uname = device.pluginProps.get('username', None)
-        passwd = device.pluginProps.get('password', None)
-        client = clients.SSHClient(address, username=uname, password=passwd)
-
-        # macOS commands are known and cannot be changed by the user
-        client.commands['status'] = '/usr/bin/true'
-        client.commands['shutdown'] = '/sbin/shutdown -h now'
-
-        self.client = client
-        self.device = device
-
-    #---------------------------------------------------------------------------
-    @staticmethod
-    def validateConfig(values, errors):
-        validateConfig_String('address', values, errors, emptyOk=False)
 
---
+++ Network Devices.indigoPlugin/Contents/Server Plugin/wrapper.py
@@ -0,0 +1,208 @@
+# provides wrapper objects for specific device types
+
+import logging
+import urllib2
+
+import clients
+
+################################################################################
+# wrapper base class for device types
+class DeviceWrapper():
+
+    #---------------------------------------------------------------------------
+    def __init__(self, device):
+        raise NotImplementedError()
+
+    #---------------------------------------------------------------------------
+    # sub-classes should override this for their specific device states
+    def updateStatus(self):
+        device = self.device
+
+        if self.client.isAvailable():
+            self.logger.debug(u'%s is AVAILABLE', device.name)
+            device.updateStateOnServer('active', True)
+            device.updateStateOnServer('status', 'Active')
+
+        else:
+            self.logger.debug(u'%s is UNAVAILABLE', device.name)
+            device.updateStateOnServer('active', False)
+            device.updateStateOnServer('status', 'Inactive')
+
+################################################################################
+# base wrapper class for relay-type devices
+class RelayDeviceWrapper(DeviceWrapper):
+
+    #---------------------------------------------------------------------------
+    def __init__(self, device):
+        raise NotImplementedError()
+
+    #---------------------------------------------------------------------------
+    # default behavior; subclasses should provide correct implementation
+    def turnOff(self):
+        self.logger.warn(u'Not supported - Turn Off %s', self.device.name)
+
+    #---------------------------------------------------------------------------
+    # default behavior; subclasses should provide correct implementation
+    def turnOn(self):
+        self.logger.warn(u'Not supported - Turn On %s', self.device.name)
+
+    #---------------------------------------------------------------------------
+    # basic check to see if the server is responding
+    def updateStatus(self):
+        device = self.device
+
+        if self.client.isAvailable():
+            self.logger.debug(u'%s is AVAILABLE', device.name)
+            device.updateStateOnServer('onOffState', 'on')
+        else:
+            self.logger.debug(u'%s is UNAVAILABLE', device.name)
+            device.updateStateOnServer('onOffState', 'off')
+
+################################################################################
+# plugin device wrapper for Network Service devices
+class Service(DeviceWrapper):
+
+    #---------------------------------------------------------------------------
+    def __init__(self, device):
+        # to emit Indigo events, logger must be a child of 'Plugin'
+        self.logger = logging.getLogger('Plugin.DeviceWrapper_Service')
+
+        address = device.pluginProps['address']
+        port = int(device.pluginProps['port'])
+        client = clients.ServiceClient(address, port)
+
+        self.device = device
+        self.client = client
+
+    #---------------------------------------------------------------------------
+    @staticmethod
+    def validateConfig(values, errors):
+        validateConfig_String('address', values, errors, emptyOk=False)
+        validateConfig_Int('port', values, errors, min=1, max=65536)
+
+################################################################################
+# plugin device wrapper for Ping Status devices
+class Ping(DeviceWrapper):
+
+    #---------------------------------------------------------------------------
+    def __init__(self, device):
+        # to emit Indigo events, logger must be a child of 'Plugin'
+        self.logger = logging.getLogger('Plugin.DeviceWrapper_Ping')
+
+        address = device.pluginProps['address']
+
+        self.device = device
+        self.client = clients.PingClient(address)
+
+    #---------------------------------------------------------------------------
+    @staticmethod
+    def validateConfig(values, errors):
+        validateConfig_String('address', values, errors, emptyOk=False)
+
+################################################################################
+# plugin device wrapper for HTTP Status devices
+class HTTP(DeviceWrapper):
+
+    #---------------------------------------------------------------------------
+    def __init__(self, device):
+        # to emit Indigo events, logger must be a child of 'Plugin'
+        self.logger = logging.getLogger('Plugin.DeviceWrapper_HTTP')
+
+        url = device.pluginProps['url']
+
+        self.device = device
+        self.client = clients.HttpClient(url)
+
+    #---------------------------------------------------------------------------
+    @staticmethod
+    def validateConfig(values, errors):
+        validateConfig_String('url', values, errors, emptyOk=False)
+
+        # update 'address' for proper display
+        url = values['url']
+        req = urllib2.Request(url)
+
+        values['address'] = req.get_host()
+
+################################################################################
+# plugin device wrapper for Local Device types
+class Local(DeviceWrapper):
+
+    #---------------------------------------------------------------------------
+    def __init__(self, device):
+        # to emit Indigo events, logger must be a child of 'Plugin'
+        self.logger = logging.getLogger('Plugin.DeviceWrapper_Local')
+
+        address = device.pluginProps['address']
+
+        self.device = device
+        self.client = clients.NullClient()
+
+    #---------------------------------------------------------------------------
+    @staticmethod
+    def validateConfig(values, errors):
+        validateConfig_String('address', values, errors, emptyOk=False)
+
+################################################################################
+# plugin device wrapper for SSH Device types
+class SSH(RelayDeviceWrapper):
+
+    #---------------------------------------------------------------------------
+    def __init__(self, device):
+        # to emit Indigo events, logger must be a child of 'Plugin'
+        self.logger = logging.getLogger('Plugin.DeviceWrapper_SSH')
+
+        address = device.pluginProps['address']
+        port = int(device.pluginProps['port'])
+        uname = device.pluginProps['username']
+        client = clients.SSHClient(address, port=port, username=uname)
+
+        client.commands['status'] = device.pluginProps['cmd_status']
+        client.commands['shutdown'] = device.pluginProps['cmd_shutdown']
+
+        self.client = client
+        self.device = device
+
+    #---------------------------------------------------------------------------
+    @staticmethod
+    def validateConfig(values, errors):
+        DeviceWrapper_Service.validateConfig(values, errors)
+        validateConfig_String('cmd_status', values, errors, emptyOk=False)
+        validateConfig_String('cmd_shutdown', values, errors, emptyOk=False)
+
+    #---------------------------------------------------------------------------
+    def turnOff(self):
+        device = self.device
+        self.logger.info(u'Shutting down %s', device.name)
+
+        if not self.client.turnOff():
+            self.logger.error(u'Could not turn off remote server: %s', device.name)
+
+################################################################################
+# plugin device wrapper for macOS Device types
+class macOS(RelayDeviceWrapper):
+
+    # XXX could we use remote management instead of SSH?
+
+    #---------------------------------------------------------------------------
+    def __init__(self, device):
+        # to emit Indigo events, logger must be a child of 'Plugin'
+        self.logger = logging.getLogger('Plugin.DeviceWrapper_macOS')
+
+        address = device.pluginProps['address']
+        uname = device.pluginProps.get('username', None)
+        passwd = device.pluginProps.get('password', None)
+        client = clients.SSHClient(address, username=uname, password=passwd)
+
+        # macOS commands are known and cannot be changed by the user
+        client.commands['status'] = '/usr/bin/true'
+        client.commands['shutdown'] = '/sbin/shutdown -h now'
+
+        self.client = client
+        self.device = device
+
+    #---------------------------------------------------------------------------
+    @staticmethod
+    def validateConfig(values, errors):
+        validateConfig_String('address', values, errors, emptyOk=False)
+
