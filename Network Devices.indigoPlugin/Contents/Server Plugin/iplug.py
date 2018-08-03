b1b996e6cd77ae6e5459cb23e0947ffe376f5d11
jheddings <jason@heddings.com>
2018-08-02 22:47:00 -0600

    Updated iPlug

--- Network Devices.indigoPlugin/Contents/Server Plugin/iplug.py
+++ Network Devices.indigoPlugin/Contents/Server Plugin/iplug.py
@@ -13,48 +13,62 @@ re_hostname = re.compile('^(\w[\-\.]?)+$')
 re_macaddr = re.compile('^([0-9a-fA-F][0-9a-fA-F][:\-]){5}([0-9a-fA-F][0-9a-fA-F])$')
 
 ################################################################################
-class ThreadedPlugin(indigo.PluginBase):
-
-    # delay between loop steps, set by plugin config
-    threadLoopDelay = None
+class PluginBase(indigo.PluginBase):
 
     #---------------------------------------------------------------------------
-    # subclasses should invoke the base __init__ if overidden
+    # NOTE: subclasses should invoke the base __init__ if overidden
     def __init__(self, pluginId, pluginDisplayName, pluginVersion, pluginPrefs):
         indigo.PluginBase.__init__(self, pluginId, pluginDisplayName, pluginVersion, pluginPrefs)
         self.loadPluginPrefs(pluginPrefs)
 
     #---------------------------------------------------------------------------
+    # NOTE: subclasses should invoke the base deviceStartComm if overidden
     def deviceStartComm(self, device):
         self.logger.debug(u'Starting device - %s [%s]', device.name, device.deviceTypeId)
 
     #---------------------------------------------------------------------------
+    # NOTE: subclasses should invoke the base deviceStopComm if overidden
     def deviceStopComm(self, device):
         self.logger.debug(u'Stopping device: %s', device.name)
 
     #---------------------------------------------------------------------------
-    # return the value from the dict as an integer, optionally providing a default value
-    def getPrefAsInt(self, prefs, name, dfault=None):
-        givenValue = prefs.get(name, None)
+    # return the value from the dict as a string, optionally providing a default value
+    def getPref(self, prefs, name, dfault=None):
+        value = prefs.get(name, None)
 
-        value = dfault
+        if value is None:
+            value = dfault
 
-        if givenValue is not None:
-            value = int(givenValue)
-
-        self.logger.debug(u'{%s} - %s', name, str(value))
+        self.logger.debug(u'prefs{%s} = %s', name, str(value))
 
         return value
 
     #---------------------------------------------------------------------------
-    # subclasses should invoke the base loadPluginPrefs if overidden
+    # return the value from the dict as an integer, optionally providing a default value
+    def getPrefAsInt(self, prefs, name, dfault=None):
+        value = self.getPref(prefs, name, dfault)
+
+        if value is not None:
+            value = int(value)
+
+        return value
+
+    #---------------------------------------------------------------------------
+    # return the value from the dict as a float, optionally providing a default value
+    def getPrefAsFloat(self, prefs, name, dfault=None):
+        value = self.getPref(prefs, name, dfault)
+
+        if value is not None:
+            value = float(value)
+
+        return value
+
+    #---------------------------------------------------------------------------
+    # NOTE: subclasses should invoke the base loadPluginPrefs if overidden
     def loadPluginPrefs(self, prefs):
         # setup logging system
         self.logLevel = self.getPrefAsInt(prefs, 'logLevel', 20)
         self.indigo_log_handler.setLevel(self.logLevel)
-
-        # save loop delay
-        self.threadLoopDelay = self.getPrefAsInt(prefs, 'threadLoopDelay', 60)
 
     #---------------------------------------------------------------------------
     # reload the plugin prefs whenever the config dialog is closed
@@ -63,25 +77,69 @@ class ThreadedPlugin(indigo.PluginBase):
 
         self.loadPluginPrefs(prefs)
 
+################################################################################
+class ThreadedPlugin(PluginBase):
+
+    # delay between loop steps, set by plugin config
+    threadLoopDelay = None
+
     #---------------------------------------------------------------------------
-    # perform the work in the thread loop for the plugin
-    # subclasses should invoke the base runLoopStep if overidden
+    # NOTE: subclasses should invoke the base loadPluginPrefs if overidden
+    def loadPluginPrefs(self, prefs):
+        PluginBase.loadPluginPrefs(self, prefs)
+
+        # save loop delay
+        self.threadLoopDelay = self.getPrefAsInt(prefs, 'threadLoopDelay', 60)
+
+    #---------------------------------------------------------------------------
+    # perform the main work in the thread loop for the plugin. the timing of
+    # this method is not guaranteed, however it is guaranteed to run once per
+    # loop iteration.  it must be overidden by sublcasses.
     def runLoopStep(self): raise NotImplementedError
+
+    #---------------------------------------------------------------------------
+    # HOOK - something to do just before the gthread loop starts
+    def preThreadLoopHook(self): pass
+
+    #---------------------------------------------------------------------------
+    # HOOK - something to do just after the thread loop stops
+    def postThreadLoopHook(self): pass
+
+    #---------------------------------------------------------------------------
+    # HOOK - perform work in the plugin thread before the loop delay
+    def preLoopDelayHook(self): pass
+
+    #---------------------------------------------------------------------------
+    # HOOK - perform work in the plugin thread after the loop delay
+    def postLoopDelayHook(self): pass
 
     #---------------------------------------------------------------------------
     def runConcurrentThread(self):
         self.logger.debug(u'Thread Started')
 
+        # allow plugins to do work before the thread starts
+        self.preThreadLoopHook()
+
         try:
 
             while not self.stopThread:
+                # perform the main work of the thread
                 self.runLoopStep()
+
+                # do plugin work before the loop delay
+                self.preLoopDelayHook()
 
                 # sleep for the configured timeout
                 self.sleep(self.threadLoopDelay)
 
+                # do plugin work after the loop delay
+                self.postLoopDelayHook()
+
         except self.StopThread:
             pass
+
+        # allow plugins to do work when the thread stops
+        self.postThreadLoopHook()
 
         self.logger.debug(u'Thread Stopped')
 
